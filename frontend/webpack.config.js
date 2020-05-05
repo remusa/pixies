71fcbe773926211a4c7b12e1737ec45c80fd0989
R Sánchez <rene.as.himself@gmail.com>
2020-05-05 12:26:17 -0500

    01 Just Enough Webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new CleanWebpackPlugin(),
+  ],
+}
