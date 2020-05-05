e5fe1430e7a36da8a4acf8712bf64b579647e2e7
R Sánchez <rene.as.himself@gmail.com>
2020-05-05 13:13:12 -0500

    04 A Bit of React

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pixies
+
+[Pixies - Heroku](https://guarded-retreat-75943.herokuapp.com/)
+
+## Resources
+
+* [Overview of all pages with the series #Pixies: Build an app with Django, React and Docker - aiki.dev](https://aiki.dev//series/pixies-build-an-app-with-django-react-and-docker/)
---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap'
+
+const App = () => {
+  const items = [
+    {
+      url:
+        'https://photos.smugmug.com/Animals/Birds/Warblers/Yellow-Warbler-2013/i-Tr5rQBN/0/f058a66f/X5/191130_Camino%20de%20grava%20entre%20el%20Corchito%20y%20Estero%20de%20Chicxulub_079-X5.jpg',
+      caption: 'image 1',
+      details: 'details 1',
+    },
+    {
+      url:
+        'https://photos.smugmug.com/Animals/Reptiles/Crocodilians/American-Crocodile/i-vkN5pCN/0/b92acda4/5K/151029_Everglades_729-5K.jpg',
+      caption: 'Image 2',
+      details: 'details 2',
+    },
+    {
+      url:
+        'https://photos.smugmug.com/Animals/Reptiles/Lizards/Pygmy-Horned-Lizard-2013/i-cdRKpxK/0/fe991315/X5/131005_ManastashRidge_693-X5.jpg',
+      caption: 'Image 3',
+      details: 'details 3',
+    },
+  ]
+
+  return (
+    <div className='container'>
+      <div className='row'>
+        <Carousel>
+          {items.map(item => (
+            <Carousel.Item>
+              <img className='d-block w-100' src={item.url} alt='' />
+
+              <Carousel.Caption>
+                <h3>{item.caption}</h3>
+                <p>{item.details}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  )
+}
+
+export default App
